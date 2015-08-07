94fb2a7dfb9aaa9b38dacb6718b39931999414a4
Arash Rasoulzadeh <arashrasoulzadeh@MacBook-Pro.local>
2015-08-07 11:52:20 +0430

    Initial Release

---
+++ Scripts/YA2D_engine.cs
@@ -0,0 +1,39 @@
+﻿//version 0.1
+//by Arash Rasoulzadeh
+//contact me : arashrasoulzadeh@gmail.com
+
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEditor;
+
+[Serializable]
+public class YA2D_engine : ScriptableObject
+{
+        public static float ver = 0.1f;
+
+
+    [MenuItem("YA2D/Create/Side Scroll Camera")]
+    public static void createSideScrollCamera()
+    {
+ 
+
+        GameObject a = new GameObject();
+        a.AddComponent<Camera>();
+        a.AddComponent<YA2D_sidescrollcamera>();
+        a.name = "side Scroll Camera";
+ 
+        //  Instantiate(a);
+
+
+    }
+
+ 
+    [MenuItem("YA2D/About")]
+	public static void About()
+    {
+        EditorUtility.DisplayDialog("YA2D","Yet another 2D framework. \n\n arashrasoulzadeh@gmail.com \n\nversion:"+ver,"okay");
+    }
+}
---
+++ Scripts/YA2D_sidescrollcamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YA2D_sidescrollcamera : MonoBehaviour {
+    public GameObject Target;
+    public float left,right,mainfov,zoomfov;
+ 
+    void LateUpdate()
+    {
+       
+            if (Target.transform.position.x > left && Target.transform.position.x < right )
+            {
+                    if (transform.GetComponent<Camera>().fieldOfView < mainfov)
+                        transform.GetComponent<Camera>().fieldOfView += 1f  * Time.deltaTime;
+                    
+                    Vector3 position = Target.transform.position;
+                    position.z = -4f;
+                    position.y = transform.position.y;
+                
+                    transform.position =position;
+                }else{
+                    //  transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                    if (transform.GetComponent<Camera>().fieldOfView > zoomfov)
+                    transform.GetComponent<Camera>().fieldOfView -= 1f  * Time.deltaTime;
+                    
+               
+    
+            }
+    
+    }
+}
